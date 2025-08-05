b57b2c37d50abbf3583fddaf396455cac982ec1d
cloudflaremj <mj@cloudflare.com>
2025-08-05 15:15:00 +0800

    Implement CRUD operations with D1 database

---
+++ src/routes/notes.route.ts.backup.ts
@@ -0,0 +1,16 @@
+import { Hono } from "hono";
+import { ContextExtended } from "../types";
+
+const notes = new Hono();
+
+notes.get("/", async (ctx: ContextExtended) => { });
+
+notes.get("/:id", async (ctx: ContextExtended) => { });
+
+notes.post("/", async (ctx: ContextExtended) => { });
+
+notes.put("/:id", async (ctx: ContextExtended) => { });
+
+notes.delete("/:id", async (ctx: ContextExtended) => { });
+
+export default notes;
